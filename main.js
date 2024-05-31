c8fec60dc3d36a0c0b4cd735804503631acecb23
alarmsock <87397257+kilohadron@users.noreply.github.com>
2024-05-31 09:51:48 -0500

    Update main.js

--- main.js
+++ main.js
@@ -10,14 +10,29 @@ var needle = {
     speed: 0,
     type: 0,
 }
-var player = {
-    h: 25,
-    w: 50,
-    x: 0,
-    y: 0,
-    rot: 0,
-    speed: 0,
+
+class shape {
+    constructor(x, y, w, h, rot, color) {
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.rot = rot
+        this.color = color
+    }
+    draw() {
+        rotate(this.h, this.w, this.x, this.y, this.rot)
+        c.fillStyle = this.color
+        c.fillRect(this.x, this.y, this.w, this.h)
+        c.resetTransform()
+    }
 }
+
+var player1 = new shape(0,0,50,25,0,"blue")
+player1.speed = 0
+var player2 = new shape(1300,0,50,25,0,"red")
+
+
 function rotate(h, w, x, y, rotation){
     c.translate(x + w / 2, y + h / 2);
     c.rotate(rotation)
@@ -26,11 +41,8 @@ function rotate(h, w, x, y, rotation){
 requestAnimationFrame(newframe)
 function newframe() {
     c.clearRect(0, 0, canvas.width, canvas.height);
-    rotate(player.h, player.w, player.x, player.y, player.rot)
-    c.fillStyle = 'red'
-    //c.fillRect(0,0,100,100)
-    c.fillRect(player.x, player.y, player.w, player.h)
-    c.resetTransform()
+    player1.draw()
+    player2.draw()
     movment()
     requestAnimationFrame(newframe)
 }
@@ -44,13 +56,31 @@ addEventListener('keydown', (e) => {
 addEventListener('keyup', (e) => {
     keys[e.key] = false
 })
-var turnspeed = (player.speed/5)*0.05
+
+function movetoward(current, target, speed) {
+    var dir = Math.sign(target - current)
+    var dist = Math.abs(target - current)
+    if (dist <= speed) return target
+    return current + dir * speed
+}
+
+var turnspeed = (player1.speed/5)*0.05
 function movment() {
-    var turnspeed = (player.speed/5)*0.05
-    if (keys['w']) if (player.speed < 30) player.speed+=0.5
-    if (keys['s']) if (player.speed > 0) player.speed-=1
-    if (keys['d']) player.rot+=turnspeed
-    if (keys['a']) player.rot-=turnspeed
-    player.y+=Math.sin(player.rot)*player.speed
-    player.x+=Math.cos(player.rot)*player.speed
+    var turnspeed = (player1.speed/5)*0.05
+
+    if (keys['w']) if (player1.speed < 20) player1.speed+=0.5
+    if (keys[' ']) if (Math.abs(player1.speed) > 0) player1.speed = movetoward(player1.speed, 0, 1)
+    if (keys['s']) if (player1.speed > -20) player1.speed-=1
+    if (keys['d']) player1.rot+=turnspeed
+    if (keys['a']) player1.rot-=turnspeed
+    //move one step
+    player1.y+=Math.sin(player1.rot)*player1.speed
+    player1.x+=Math.cos(player1.rot)*player1.speed
+}
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+})
+function wallcollisions(){
+    
 }
