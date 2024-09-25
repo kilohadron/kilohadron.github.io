d3691a772f3170aa0185a2d660801ff22218657e
alarmsock <87397257+kilohadron@users.noreply.github.com>
2024-09-25 20:20:52 +0000

    ahhhh ahhhh achooo

--- js/main.js
+++ js/main.js
@@ -25,11 +25,13 @@ function newframe() {
     lastframetime = Date.now()
     //delta time
     c.clearRect(0, 0, canvas.width, canvas.height)
-    movement(playr1)
+    //movement(playr1)
+    movement(playr2)
     playr1.draw()
     playr2.draw()
     collision(playr1,playr2)
-    vertDebug()
+    vertDebug(playr1)
+    vertDebug(playr2)
     requestAnimationFrame(newframe)
 }
 
@@ -42,11 +44,11 @@ function drawCirc(x,y,radius){
     c.arc(x,y,radius, 0, 2 * Math.PI);
     c.stroke();
 }
-function vertDebug(){
-    drawCirc(playr1.vert[0].x,playr1.vert[0].y,1)
-    drawCirc(playr1.vert[1].x,playr1.vert[1].y,1)
-    drawCirc(playr1.vert[2].x,playr1.vert[2].y,1)
-    drawCirc(playr1.vert[3].x,playr1.vert[3].y,1)
+function vertDebug(box){
+    drawCirc(box.vert[0].x,box.vert[0].y,1)
+    drawCirc(box.vert[1].x,box.vert[1].y,1)
+    drawCirc(box.vert[2].x,box.vert[2].y,1)
+    drawCirc(box.vert[3].x,box.vert[3].y,1)
 }
 window.addEventListener('resize', () => {
     canvas.width = window.innerWidth
@@ -70,8 +72,7 @@ window.addEventListener('resize', () => {
 
 
 
-
-
+const distance = (p1,p2) => Math.sqrt((Math.pow((p2.x-p1.x),2) + Math.pow((p2.y-p1.y),2)))
 class playr {
     constructor(x, y, w, h, rot, color) {
         this.x = x
@@ -104,7 +105,8 @@ var playr1 = new playr(0,cy,50,25,0,"blue")
 var playr2 = new playr(canvas.width-60,cy,50,25,Math.PI,"red")
 
 function movement(box) {
-    if (box.center.x<0||box.center.y<0) alert('out of bounds')
+    if (box.center.x<0||box.center.y<0||box.center.x>canvas.width||box.center.y>canvas.height) {box.rot = Math.atan2(canvas.height/2-box.y,canvas.width/2-box.x)
+    box.speed = 750}
     var totalspeed = (box.speed/750)
     if (keys['w']) if (box.speed < 750) box.speed+=250*deltatime 
     if (keys[' ']) if (Math.abs(box.speed) > 0) box.speed = movetoward(box.speed, 0, 500*deltatime)
