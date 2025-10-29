6cbc6174577c5401161d15335fb5a660850bbf85
alarmsock <87397257+kilohadron@users.noreply.github.com>
2025-10-29 14:24:35 -0500

    Refactor player objects and control logic

--- js/game.js
+++ js/game.js
@@ -1,37 +1,67 @@
-blue = {
-    sprite:new rect({x:20,y:canvas.height/2,h:10,w:20,r:0,color:"blue"}),
+const controls = [
+    {up:'e',down:'d',left:'s',right:'f',special:'z'},
+    {up:'i',down:'k',left:'j',right:'l',special:'/'}
+]
+const p1 = {
     speed:0,
-    spike:new rect({x:0,y:0,h:2.5,w:26,r:0,color:"white"}),
-    control:function(){
-        this.sprite.r += (keys[controls[0].right] ? Math.PI/180*this.speed/3 : 0) - (keys[controls[0].left] ? Math.PI/180*this.speed/3 : 0);
-        this.speed += (keys[controls[0].up] && this.speed <= 25 ? 1 : 0) - (keys[controls[0].down]&&this.speed<= 25 ? 1 : 0);
-        this.speed /= (keys[' '] ? 1.5 : 1);
-        this.sprite.slide(this.speed)
-        this.spike.x = this.sprite.x + Math.cos(this.sprite.r)*this.sprite.w*2
-        this.spike.y = this.sprite.y + Math.sin(this.sprite.r)*this.sprite.w*2
-        this.spike.r = this.sprite.r
-    },
+    body:new Box('Indigo',25,resolution.height/2,25,50),
+    needle:new Box('cornsilk',0,0,9,50),
     draw:function(){
-        this.sprite.draw();
-        this.spike.draw();
+        this.needle.x = this.body.x
+        this.needle.y = this.body.y
+        this.needle.r = this.body.r
+        this.needle.slide(this.body.w/2+this.needle.w/2)
+        this.body.draw()
+        this.needle.draw()
+    },
+    move:function(){
+        ifOnEdgeBounce(this)
+        if(keys[controls[0].special]){
+            this.speed /= 1.1
+        }
+        else{
+            this.speed += keys[controls[0].up]&&!keys[controls[0].down]?1:keys[controls[0].down]&&!keys[controls[0].up]?-1:0
+        }
+        if(keys[controls[0].left]){
+            this.body.r -= Math.PI/180 *5
+        }    
+        if(keys[controls[0].right]){
+            this.body.r += Math.PI/180 *5
+        }  
+        this.body.slide(this.speed)
+        
     }
 }
 
-red = {
-    sprite:new rect({x:canvas.width-20,y:canvas.height/2,h:10,w:20,r:180,color:"red"}),
+const p2 = {
     speed:0,
-    spike:new rect({x:0,y:0,h:2.5,w:26.25,r:0,color:"white"}),
-    control:function(){
-        this.sprite.r += (keys[controls[1].right] ? Math.PI/180*this.speed/3 : 0) - (keys[controls[1].left] ? Math.PI/180*this.speed/3 : 0);
-        this.speed += (keys[controls[1].up]&&this.speed<= 25 ? 1 : 0) - (keys[controls[1].down]&&this.speed<= 25 ? 1 : 0);
-        this.speed /= (keys['Shift'] ? 1.5 : 1);
-        this.sprite.slide(this.speed)
-        this.spike.x = this.sprite.x + Math.cos(this.sprite.r)*this.sprite.w*2
-        this.spike.y = this.sprite.y + Math.sin(this.sprite.r)*this.sprite.w*2
-        this.spike.r = this.sprite.r
-    },
+    body:new Box('crimson',resolution.width-25,resolution.height/2,25,50),
+    needle:new Box('cornsilk',0,0,9,50),
     draw:function(){
-        this.sprite.draw();
-        this.spike.draw();
+        this.needle.x = this.body.x
+        this.needle.y = this.body.y
+        this.needle.r = this.body.r
+        this.needle.slide(this.body.w/2+this.needle.w/2)
+        this.body.draw()
+        this.needle.draw()
+    },
+    move:function(){
+        ifOnEdgeBounce(this)
+        if(keys[controls[1].special]){
+            this.speed /= 1.1
+        }
+        else{
+            this.speed += keys[controls[1].up]&&!keys[controls[1].down]?1:keys[controls[1].down]&&!keys[controls[1].up]?-1:0
+        }
+        if(keys[controls[1].left]){
+            this.body.r -= Math.PI/180 *5
+        }    
+        if(keys[controls[1].right]){
+            this.body.r += Math.PI/180 *5
+        }  
+        this.body.slide(this.speed)
+        
     }
 }
+p2.body.r = Math.PI 
+requestAnimationFrame(main);
